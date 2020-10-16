c368e64a4c2508a3d948c0e5204146eb52450b07
lucianne302 <lucianne21@comcast.net>
2020-10-16 19:16:30 -0400

    trying to add router uses for zookeeperRoutes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const router = require('express').Router();
+// const animalRoutes = require('../apiRoutes/animalRoutes');
+
+// router.use(animalRoutes);
+
+//router.use(require('http://localhost:3001/routes/apiRoutes/zookeeperRoutes'));
+router.use(require('../apiRoutes/zookeeperRoutes'));
+
+//router.use(require('./zookeeperRoutes'));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+module.exports = router;
